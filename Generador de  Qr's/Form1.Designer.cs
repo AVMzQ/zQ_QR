99ac8b6ce2e6667ce92c6797029c68f842841ed5
zQ <angel.vazquez1253@gmail.com>
2023-07-27 20:23:06 -0600

    add code commit

---
+++ Generador de  Qr's/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Generador_de__Qr_s
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pbCodigoQr = new System.Windows.Forms.PictureBox();
+            this.rdbEnlace = new System.Windows.Forms.RadioButton();
+            this.rdbNormal = new System.Windows.Forms.RadioButton();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.txtContenido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodigoQr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pbCodigoQr);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.rdbEnlace);
+            this.splitContainer1.Panel2.Controls.Add(this.rdbNormal);
+            this.splitContainer1.Panel2.Controls.Add(this.btnGenerar);
+            this.splitContainer1.Panel2.Controls.Add(this.txtContenido);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Panel2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.splitContainer1.Size = new System.Drawing.Size(318, 382);
+            this.splitContainer1.SplitterDistance = 180;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pbCodigoQr
+            // 
+            this.pbCodigoQr.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbCodigoQr.Location = new System.Drawing.Point(0, 0);
+            this.pbCodigoQr.Name = "pbCodigoQr";
+            this.pbCodigoQr.Size = new System.Drawing.Size(314, 176);
+            this.pbCodigoQr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbCodigoQr.TabIndex = 0;
+            this.pbCodigoQr.TabStop = false;
+            // 
+            // rdbEnlace
+            // 
+            this.rdbEnlace.AutoSize = true;
+            this.rdbEnlace.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbEnlace.Location = new System.Drawing.Point(16, 47);
+            this.rdbEnlace.Name = "rdbEnlace";
+            this.rdbEnlace.Size = new System.Drawing.Size(148, 22);
+            this.rdbEnlace.TabIndex = 4;
+            this.rdbEnlace.TabStop = true;
+            this.rdbEnlace.Text = "Enlace (pagina web)";
+            this.rdbEnlace.UseVisualStyleBackColor = true;
+            this.rdbEnlace.CheckedChanged += new System.EventHandler(this.rdbEnlace_CheckedChanged);
+            // 
+            // rdbNormal
+            // 
+            this.rdbNormal.AutoSize = true;
+            this.rdbNormal.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbNormal.Location = new System.Drawing.Point(16, 13);
+            this.rdbNormal.Name = "rdbNormal";
+            this.rdbNormal.Size = new System.Drawing.Size(133, 22);
+            this.rdbNormal.TabIndex = 3;
+            this.rdbNormal.TabStop = true;
+            this.rdbNormal.Text = "Contenido normal.";
+            this.rdbNormal.UseVisualStyleBackColor = true;
+            this.rdbNormal.CheckedChanged += new System.EventHandler(this.rdbNormal_CheckedChanged);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnGenerar.Location = new System.Drawing.Point(81, 149);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(159, 35);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar codigo";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // txtContenido
+            // 
+            this.txtContenido.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtContenido.Location = new System.Drawing.Point(10, 113);
+            this.txtContenido.Name = "txtContenido";
+            this.txtContenido.Size = new System.Drawing.Size(301, 30);
+            this.txtContenido.TabIndex = 1;
+            this.txtContenido.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtContenido_MouseClick);
+            this.txtContenido.TextChanged += new System.EventHandler(this.txtContenido_TextChanged);
+            this.txtContenido.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtContenido_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Agregar contenido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 382);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(334, 399);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generador Qr";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCodigoQr)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pbCodigoQr;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox txtContenido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rdbEnlace;
+        private System.Windows.Forms.RadioButton rdbNormal;
+    }
+}
+
---
+++ Generador de  Qr's/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+using System.IO;
+using System.Windows.Shapes;
+using static QRCoder.PayloadGenerator;
+
+namespace Generador_de__Qr_s
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtContenido.Text != "")
+                {
+                    if (rdbNormal.Checked)
+                    {
+                        using (SaveFileDialog s = new SaveFileDialog())
+                        {
+                            s.FileName = txtContenido.Text;
+                            s.DefaultExt = ".Png";
+                            s.Filter = "Image(.Png)|*.Png";
+                            if (s.ShowDialog() == DialogResult.OK)
+                            {
+                                // Save Image
+                                string filename = s.FileName;
+                                FileStream fstream = new FileStream(filename, FileMode.Create);
+                                pbCodigoQr.Image.Save(fstream, System.Drawing.Imaging.ImageFormat.Png);
+                                fstream.Close();
+                            }
+                        }
+                        MessageBox.Show("Codigo Generado con exito!");
+                        txtContenido.Clear();
+                    }
+                    else if(rdbEnlace.Checked)
+                    {
+                        using (SaveFileDialog s = new SaveFileDialog())
+                        {
+                            s.FileName = "Enlace";
+                            s.DefaultExt = ".Png";
+                            s.Filter = "Image(.Png)|*.Png";
+                            if (s.ShowDialog() == DialogResult.OK)
+                            {
+                                // Save Image
+                                string filename = s.FileName;
+                                FileStream fstream = new FileStream(filename, FileMode.Create);
+                                pbCodigoQr.Image.Save(fstream, System.Drawing.Imaging.ImageFormat.Png);
+                                fstream.Close();
+                            }
+                            MessageBox.Show("Codigo Generado con exito!");
+                            txtContenido.Clear();
+                        }
+                    }
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Error> " + x.Message);
+            }
+        }
+
+        private void rdbNormal_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbNormal.Checked)
+            {
+                txtContenido.Clear();
+                rdbEnlace.Checked = false;
+            }
+        }
+
+        private void rdbEnlace_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbEnlace.Checked)
+            {
+                txtContenido.Clear();
+                rdbNormal.Checked = false;
+            }
+        }
+
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = Application.StartupPath + "\\CodigoQr\\";
+                System.Diagnostics.Process.Start(@path);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Error> " + x.Message);
+            }
+        }*/
+
+        private void txtContenido_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtContenido.Text != "")
+                {
+                    if (rdbNormal.Checked)
+                    {
+                        //Contenido normal
+                        using (QRCodeGenerator qr = new QRCodeGenerator())
+                        {
+                            QRCodeData qrData = qr.CreateQrCode(txtContenido.Text, QRCodeGenerator.ECCLevel.Q);
+                            QRCode qrCode = new QRCode(qrData);
+
+                            //Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile("C:\\myimage.png"));
+                            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile(Application.StartupPath + "\\zQ.png"));
+
+                            pbCodigoQr.Image = qrCodeImage;
+                        }
+                    }
+                    else if (rdbEnlace.Checked)
+                    {
+                        //Enlace
+                        using (QRCodeGenerator qr = new QRCodeGenerator())
+                        {
+                            Url generator = new Url(txtContenido.Text);
+                            string payload = generator.ToString();
+                            QRCodeData qrData = qr.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
+                            QRCode qrCode = new QRCode(qrData);
+                            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile(Application.StartupPath + "\\zQ.png"));
+                            pbCodigoQr.Image = qrCodeImage;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Selecciona un modo primero!");
+                        txtContenido.Clear();
+                    }
+                }
+                else
+                {
+                    pbCodigoQr.Image = null;
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Error> " + x.Message);
+            }
+        }
+        private void txtContenido_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtContenido.SelectAll();
+        }
+
+        private void txtContenido_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+                btnGenerar.Focus();
+        }
+    }
+}
